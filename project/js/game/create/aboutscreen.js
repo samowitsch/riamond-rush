7a9185fad3a238fe81d555dfc521011962da20b9
Christian Sonntag <info@motions-media.de>
2024-03-22 20:22:39 +0100

    about screen improvements

--- project/js/game/create/aboutscreen.js
+++ project/js/game/create/aboutscreen.js
@@ -9,7 +9,7 @@ GameCreate.prototype.aboutscreenObjects = function () {
         x: 1400,
         y: 70,
         rotation: -0.02,
-        scale: 0.2
+        scale: 0.4
     })
     Game.entities.push(riamond)
 
@@ -18,35 +18,41 @@ GameCreate.prototype.aboutscreenObjects = function () {
 
     var aboutText = new Bitmap({
         x: can.width / 2,
-        y: can.height / 2,
+        y: can.height / 2 - 100,
         width: 700,
         height: 400
     })
 
     // draw text to bitmap context
     Game.font.drawMultiLineText({
-        text: 'RIAMOND RUSH (c) 2014\n\n' +
-        'PROGRAMMING: CHRISTIAN SONNTAG\n' +
-        'LEVELS: CHRISTIAN+MONA SONNTAG, NICO GREIN\n' +
-        'TESTING: CHRISTIAN+MONA SONNTAG, ALEX GREIN\n' +
-        'SOUND: www.nosoapradio.us\n\n' +
-        'PROUDLY WRITTEN IN VANILLA JAVASCRIPT\n' +
-        'POWERED BY LUDEI\n\n' +
-        'CONTACT: www.motions-media.de',
+        text: 'RIAMOND RUSH (c) 2014, 2024\n\n' +
+            'PROGRAMMING: CHRISTIAN SONNTAG\n' +
+            'LEVELS: CHRISTIAN+MONA SONNTAG, NICO GREIN\n' +
+            'TESTING: CHRISTIAN+MONA SONNTAG, ALEX GREIN\n' +
+            'SOUND: www.nosoapradio.us\n' +
+            'CONTACT: www.motions-media.de\n\n' +
+            'PROUDLY WRITTEN IN VANILLA JAVASCRIPT\n',
         xpos: 0,
         ypos: Game.font.getLineHeight(),
-        lineOffset: 3,
+        lineOffset: 2,
         drawCtx: aboutText.ctx
     })
 
     Game.entities.push(aboutText)
+
+    controls = new Sprite({
+        imagename: 'gfx/control.png',
+        x: can.width / 2,
+        y: can.height - 200
+    })
+    Game.entities.push(controls)
 
 
     btnBackToStartScreen = new Button({
         atlasimage: atlasUI.image,
         atlasdata: atlasUI.getPicByName('btn-back'),
         x: -200,
-        y: can.height - 125,
+        y: can.height - 50,
         audio: btnSound
     })
     Game.entities.push(btnBackToStartScreen)
